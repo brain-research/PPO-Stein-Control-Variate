2a139da0b734033628d2aebef2d38edfc73e6f6d
Surya Bhupatiraju <sbhupatiraju@sbhupatiraju.mtv.corp.google.com>
2018-02-07 11:48:05 -0800

    fix unforunate logging

--- optimization/run_i20.sh
+++ optimization/run_i20.sh
@@ -1,11 +1,11 @@
-python train.py Humanoid-v1 -b 10000 -ps small -c 0 -s 343 --dir-name=halfcheetah_baseline_121 &
-python train.py HalfCheetah-v1 -b 10000 -ps large -c 0 -s 454 --dir-name=halfcheetah_baseline_121 &
-python train.py Humanoid-v1 -b 10000 -ps small -c 0 -s 454 --dir-name=halfcheetah_baseline_121 &
-python train.py HalfCheetah-v1 -b 10000 -ps large -c 0 -s 565 --dir-name=halfcheetah_baseline_121 &
-python train.py Humanoid-v1 -b 10000 -ps small -c 0 -s 565 --dir-name=halfcheetah_baseline_121 &
+python train.py Humanoid-v1 -b 10000 -ps small -c 0 -s 343 --dir-name=humanoid_baseline_343 &
+python train.py HalfCheetah-v1 -b 10000 -ps large -c 0 -s 454 --dir-name=halfcheetah_baseline_454 &
+python train.py Humanoid-v1 -b 10000 -ps small -c 0 -s 454 --dir-name=humanoid_baseline_454 &
+python train.py HalfCheetah-v1 -b 10000 -ps large -c 0 -s 565 --dir-name=halfcheetah_baseline_565 &
+python train.py Humanoid-v1 -b 10000 -ps small -c 0 -s 565 --dir-name=humanoid_baseline_565 &
 
-python train.py HalfCheetah-v1 -b 10000 -ps large -c 0 -s 121 --dir-name=halfcheetah_baseline_121 &
-python train.py HalfCheetah-v1 -b 10000 -ps large -c 0 -s 144 --dir-name=halfcheetah_baseline_144 &
-python train.py HalfCheetah-v1 -b 10000 -ps large -c 0 -s 169 --dir-name=halfcheetah_baseline_169 &
-python train.py HalfCheetah-v1 -b 10000 -ps large -c 0 -s 196 --dir-name=halfcheetah_baseline_196 &
-python train.py HalfCheetah-v1 -b 10000 -ps large -c 0 -s 225 --dir-name=halfcheetah_baseline_225 &
+# python train.py Walker2d-v1 -b 10000 -ps large -c 0 -s 121 --dir-name=walker2d_baseline_121 &
+# python train.py Walker2d-v1 -b 10000 -ps large -c 0 -s 144 --dir-name=walker2d_baseline_144 &
+# python train.py Walker2d-v1 -b 10000 -ps large -c 0 -s 169 --dir-name=walker2d_baseline_169 &
+# python train.py Walker2d-v1 -b 10000 -ps large -c 0 -s 196 --dir-name=walker2d_baseline_196 &
+# python train.py Walker2d-v1 -b 10000 -ps large -c 0 -s 225 --dir-name=walker2d_baseline_225 &
--- optimization/run_i21.sh
+++ optimization/run_i21.sh
@@ -1,18 +1,18 @@
-python train.py Walker2d-v1 -b 10000 -ps large -po MinVar -p 500 -s 121  --dir-name=walker2d__121 &
-python train.py Walker2d-v1 -b 10000 -ps large -po MinVar -p 500 -s 144  --dir-name=walker2d__144 &
-python train.py Walker2d-v1 -b 10000 -ps large -po MinVar -p 500 -s 169  --dir-name=walker2d__169 &
-python train.py Walker2d-v1 -b 10000 -ps large -po MinVar -p 500 -s 196  --dir-name=walker2d__196 &
-python train.py Walker2d-v1 -b 10000 -ps large -po MinVar -p 500 -s 225  --dir-name=walker2d__225 &
+# python train.py Walker2d-v1 -b 10000 -ps large -po MinVar -p 500 -s 121  --dir-name=walker2d__121 &
+# python train.py Walker2d-v1 -b 10000 -ps large -po MinVar -p 500 -s 144  --dir-name=walker2d__144 &
+# python train.py Walker2d-v1 -b 10000 -ps large -po MinVar -p 500 -s 169  --dir-name=walker2d__169 &
+# python train.py Walker2d-v1 -b 10000 -ps large -po MinVar -p 500 -s 196  --dir-name=walker2d__196 &
+# python train.py Walker2d-v1 -b 10000 -ps large -po MinVar -p 500 -s 225  --dir-name=walker2d__225 &
 
-python train.py Walker2d-v1 -b 10000 -ps large -po MinVar -p 500 -s 121 --extra-sample --dir-name=walker2d_extra-sample_121 &
-python train.py Walker2d-v1 -b 10000 -ps large -po MinVar -p 500 -s 144 --extra-sample --dir-name=walker2d_extra-sample_144 &
-python train.py Walker2d-v1 -b 10000 -ps large -po MinVar -p 500 -s 169 --extra-sample --dir-name=walker2d_extra-sample_169 &
-python train.py Walker2d-v1 -b 10000 -ps large -po MinVar -p 500 -s 196 --extra-sample --dir-name=walker2d_extra-sample_196 &
-python train.py Walker2d-v1 -b 10000 -ps large -po MinVar -p 500 -s 225 --extra-sample --dir-name=walker2d_extra-sample_225 &
+# python train.py Walker2d-v1 -b 10000 -ps large -po MinVar -p 500 -s 121 --extra-sample --dir-name=walker2d_extra-sample_121 &
+# python train.py Walker2d-v1 -b 10000 -ps large -po MinVar -p 500 -s 144 --extra-sample --dir-name=walker2d_extra-sample_144 &
+# python train.py Walker2d-v1 -b 10000 -ps large -po MinVar -p 500 -s 169 --extra-sample --dir-name=walker2d_extra-sample_169 &
+# python train.py Walker2d-v1 -b 10000 -ps large -po MinVar -p 500 -s 196 --extra-sample --dir-name=walker2d_extra-sample_196 &
+# python train.py Walker2d-v1 -b 10000 -ps large -po MinVar -p 500 -s 225 --extra-sample --dir-name=walker2d_extra-sample_225 &
 
-python train.py Walker2d-v1 -b 10000 -ps large -po MinVar -p 500 -s 121 --unbiased --dir-name=walker2d_unbiased_121 &
-python train.py Walker2d-v1 -b 10000 -ps large -po MinVar -p 500 -s 144 --unbiased --dir-name=walker2d_unbiased_144 &
-python train.py Walker2d-v1 -b 10000 -ps large -po MinVar -p 500 -s 169 --unbiased --dir-name=walker2d_unbiased_169 &
-python train.py Walker2d-v1 -b 10000 -ps large -po MinVar -p 500 -s 196 --unbiased --dir-name=walker2d_unbiased_196 &
-python train.py Walker2d-v1 -b 10000 -ps large -po MinVar -p 500 -s 225 --unbiased --dir-name=walker2d_unbiased_225 &
+# python train.py Walker2d-v1 -b 10000 -ps large -po MinVar -p 500 -s 121 --unbiased --dir-name=walker2d_unbiased_121 &
+# python train.py Walker2d-v1 -b 10000 -ps large -po MinVar -p 500 -s 144 --unbiased --dir-name=walker2d_unbiased_144 &
+# python train.py Walker2d-v1 -b 10000 -ps large -po MinVar -p 500 -s 169 --unbiased --dir-name=walker2d_unbiased_169 &
+# python train.py Walker2d-v1 -b 10000 -ps large -po MinVar -p 500 -s 196 --unbiased --dir-name=walker2d_unbiased_196 &
+# python train.py Walker2d-v1 -b 10000 -ps large -po MinVar -p 500 -s 225 --unbiased --dir-name=walker2d_unbiased_225 &
 
