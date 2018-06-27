36e53a11b08f23e20b1997915643cece21d2c2ed
George Tucker <gjt@google.com>
2018-06-27 14:20:22 -0700

    Adding in variance code from other repo

--- evaluation/traj_visualize.py
+++ evaluation/traj_visualize.py
@@ -1,10 +1,20 @@
-import numpy as np 
 import pickle
 import os 
 import errno
+import os
+import sys
+import numpy as np
+import json
+from scipy.signal import savgol_filter
 import matplotlib
 matplotlib.use('Agg')
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+import seaborn as sns
+matplotlib.rc('text', usetex=True)
+import matplotlib.patches as mpatches
+
+color_list = sns.color_palette("muted")
+sns.palplot(color_list)
 
 def mkdir_p(path):
     try:
@@ -15,147 +25,105 @@ def mkdir_p(path):
         else:
             raise
 
+def load_grad(file_path, fields=['mu_grad', 'sigma_grad']):
+  with open(file_path, 'rb') as f:
+    traj_data = pickle.load(f)
+    sample_grads = np.concatenate(
+        [traj_data[field] for field in fields], axis=1)
+
+    return sample_grads
+
 def load_sample_grads(batch_range, prefix_dir):
     file_dir = prefix_dir
 
     # load mc traj
-    stein_phi_loss = []
-    mc_phi_loss = []
     mc_grads = []
     for i in batch_range:
         file_path = os.path.join(file_dir, 'mc_num_episode=%d.pkl'%i)
-        with open(file_path, 'rb') as f:
-            traj_data = pickle.load(f)
-            sample_grads = np.concatenate([traj_data['mu_grad'], 
-                                        traj_data['sigma_grad']], axis=1)
-            
-            
-            mc_grads.append(sample_grads)
-            mc_phi_loss.append(traj_data['phi_loss'])
+        mc_grads.append(load_grad(file_path))
 
+
+    ai_grads = []
     stein_grads = []
     for i in batch_range:
         file_path = os.path.join(file_dir, 'stein_num_episode=%d.pkl'%i)
-        with open(file_path, 'rb') as f:
-            traj_data = pickle.load(f)
-            sample_grads = np.concatenate([traj_data['mu_grad'], 
-                                        traj_data['sigma_grad']], axis=1)
+        ai_grads.append(load_grad(file_path,
+                                  ['mu_ai_grad', 'sigma_ai_grad']))
+        stein_grads.append(load_grad(file_path))
 
-            stein_grads.append(sample_grads)
-            stein_phi_loss.append(traj_data['phi_loss'])
+    return mc_grads, ai_grads, stein_grads
 
-    return mc_grads, stein_grads, mc_phi_loss, stein_phi_loss
+legend_handles = {}
 
+def plot(phi_obj, batch_range=range(10, 20, 10), max_timesteps=500, seeds=[13], env_name='Walker2d-v1'):
+  k = 2000
+  plot_stein_loss = []
+  plot_mc_loss = []
 
-def gen_index(indices, max_length):
-    total_indices = []
+  for seed in seeds:
+      prefix_dir = 'max_timesteps=%s_eval_data/%s_%s_data_seed=%d_max-steps=%s'%(max_timesteps, env_name, phi_obj, seed, max_timesteps)
+      print(prefix_dir)
 
-    for index in indices:
-        total_indices.append(np.arange(index*max_length, (index+1)* max_length))
-    
-    return np.concatenate(total_indices, axis=0)
+      # This is gradient for each trajectory
+      mc_x = []
+      stein_x = []
+      plot_stein_vars = []
+      plot_mc_vars = []
 
+      mc_grads, ai_grads, stein_grads = load_sample_grads(batch_range, prefix_dir)
+
+      # Calculate variance
+      grads = [mc_grads, ai_grads, stein_grads]
+      variances = [[0]*len(grad) for grad in grads]
+
+      x = []
+      for i in range(len(mc_grads)):
+        n_samples = len(mc_grads[i])  # all trajs are concatenated together
+        x.append(n_samples)
+
+        for _ in range(k):
+          indices = np.random.choice(n_samples, int(n_samples/2), replace=False)
+          total_indices = np.arange(0,  n_samples)
+          mask = np.zeros(total_indices.shape, dtype=bool)
+          mask[indices] = True
+
+          for j, grad in enumerate(grads):
+            g = np.array(grad[i])
+            var = np.sum((np.mean(g[total_indices[mask], :], axis=0) -
+                          np.mean(g[total_indices[~mask], :], axis=0)) ** 2)
+            variances[j][i] += var/k
+
+      print (seed)
+      print(x)
+      labels = ['Value', '%s-MLP (state only)' % phi_obj, '%s-MLP' % phi_obj]
+      for plot_i, (variance, label) in enumerate(zip(variances, labels)):
+        print(label)
+        print(np.log(variance))
+        if label not in legend_handles:
+          legend_handles[label] = (mpatches.Patch(color=color_list[len(legend_handles)], label=label),
+                                   color_list[len(legend_handles)])
+        plt.plot(x, np.log(variance), color=legend_handles[label][1], label=label)
+      plt.tick_params(labelsize=11)
+      plt.ylabel('ln(Variance Proxy)', fontsize=16)
+      plt.xlabel('Sample size', fontsize=16)
+      plt.title('Walker2d-v1 %s' % phi_obj, fontsize=18)
+      plt.grid(alpha=0.5)
+      #plt.legend()
+
+  return legend_handles
 
 if __name__ == '__main__':
+  plt.figure(figsize=(20,6))
+  plt.subplot(1, 2, 1)
+  plot('FitQ', batch_range=range(10, 40, 10))
+  plt.subplot(1, 2, 2)
+  legend_handles = plot('MinVar', batch_range=range(10, 80, 10))
 
-    batch_range= range(10, 70, 10)
+  plt.legend(handles=[x for _, (x, _) in sorted(legend_handles.items(), key=lambda x: x[0])],
+             loc='upper center', bbox_to_anchor=(-0.10, -0.13),
+             ncol=6,
+             prop={'size': 14})
 
-    env_name = 'Walker2d-v1'
+  mkdir_p('results')
+  plt.savefig('results/stein.pdf', bbox_inches='tight')
 
-    try:
-        seeds = [int(x) for x in input("Enter seeds of evaluation saved data, seperated by space\n").split()]
-    except (SyntaxError, ValueError):
-        seeds = list(range(13, 253, 30))
-    if not seeds:
-        seeds = list(range(13, 253, 30))        
-    print(seeds)
-
-    try:
-        phi_obj = input("Enter type of evaluation, FitQ or MinVar: \n")
-    except (SyntaxError, ValueError):
-        print("no choice about evaluation type")
-        phi_obj = 'FitQ'
-
-    try: 
-        max_timesteps = input("Enter max_timesteps: \n")
-    except (SyntaxError, ValueError):
-        print("no choice about max_timesteps")        
-        max_timesteps = 50
-    
-    k = 20000
-    plot_stein_loss = []
-    plot_mc_loss = []
-                                 
-    for seed in seeds:
-
-        prefix_dir = 'max_timesteps=%s_eval_data/%s_%s_data_seed=%d_max-steps=%s'%(max_timesteps, env_name, phi_obj, seed, max_timesteps)
-    
-        print(prefix_dir) 
-
-        # This is gradient for each trajectory
-        mc_x = []
-        stein_x = []
-        plot_stein_vars = []
-        plot_mc_vars = []
-    
-        mc_grads, stein_grads, mc_phi_loss, \
-                    stein_phi_loss = load_sample_grads(batch_range, prefix_dir)
-
-        for mc_grad, stein_grad in zip(mc_grads, stein_grads):
-        
-            mc_x.append(len(mc_grad))
-            stein_x.append(len(stein_grad))
-            print(len(mc_grad))
-            # Calculate MSE/Variance
-            mc_vars = []
-            mc_num_traj = len(mc_grad)
-            for kk in range(k):
-                
-                indices = np.random.choice(mc_num_traj, int(mc_num_traj/2), replace=False)
-                total_indices = np.arange(0,  mc_num_traj)
-                mask = np.zeros(total_indices.shape, dtype=bool)
-                mask[indices] = True
-            
-                mc_grad = np.array(mc_grad)
-                mc_var = (np.mean(mc_grad[total_indices[mask]], axis=0) - \
-                        np.mean(mc_grad[total_indices[~mask]], axis=0)) ** 2
-                mc_vars.append(np.sum(mc_var))
-
-            plot_mc_vars.append(np.mean(mc_vars))
-
-    
-            stein_vars = []
-            stein_num_traj = len(stein_grad)
-
-            for kk in range(k):
-                
-                indices = np.random.choice(stein_num_traj, int(stein_num_traj/2), replace=False)
-                total_indices = np.arange(0, stein_num_traj)
-                mask = np.zeros(total_indices.shape, dtype=bool)
-                mask[indices] = True
-                stein_grad = np.array(stein_grad)
-                stein_var = (np.mean(stein_grad[total_indices[mask]], axis=0) - \
-                             np.mean(stein_grad[total_indices[~mask]], axis=0)) ** 2
-                stein_vars.append(np.sum(stein_var))
-        
-            plot_stein_vars.append(np.mean(stein_vars))
-
-        print (seed)
-        print (mc_x)
-        print (stein_x)
-        print (np.log(plot_stein_vars))
-        print (np.log(plot_mc_vars))
-        plt.plot(np.log(mc_x), np.log(plot_mc_vars), label='mc')
-        plt.plot(np.log(stein_x), np.log(plot_stein_vars), label='stein')
-        plt.legend()
-        mkdir_p('results')
-        plt.savefig('results/' + '%s_avg_variance_seed=%s_max-steps=%s_phi_obj=%s.pdf'%(env_name, seed, max_timesteps, phi_obj))
-        plt.gcf().clear()
-
-
-
-
-    
-
-
-        
--- evaluation/walker2d_train_eval.sh
+++ evaluation/walker2d_train_eval.sh
@@ -6,20 +6,20 @@
 # eval on a saved policy
 for ((s=13; s<=33; s+=30)) # evaluate on one seed to save time
     do
-    	for ((i=10; i<=80; i+=10)) # few number of trajectories to save time
+    	for ((i=10; i<=40; i+=10)) # few number of trajectories to save time
             do
 		# load Stein PPO and compute variance and save
 		
 		# FitQ
 		j=$((50*$i))
 		echo $j
-		python run.py Walker2d-v1 -ps large -p $j -c 1 -n $i -sha load -m 500 -s $s -po FitQ & 
-		sleep 1.5s
+		python run.py Walker2d-v1 -ps large -p $j -c 1 -n $i -sha load -m 1000 -s $s -po FitQ
+#		sleep 1.5s
 
-		# MinVar
+#		# MinVar
 		k=$((50*$i))
 		echo $k
-		python run.py Walker2d-v1 -ps large -p $k -c 1 -n $i -sha load -m 500 -s $s -po MinVar &
-		sleep 1.5s
+		python run.py Walker2d-v1 -ps large -p $k -c 1 -n $i -sha load -m 1000 -s $s -po MinVar
+#		sleep 1.5s
             done
     done 
